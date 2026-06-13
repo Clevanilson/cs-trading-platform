3a6bf4e859217c3e45cf931ba67459d94bcc6efc
Clevanilson <clevanilson.contato@gmail.com>
2026-06-13 14:54:15 -0300

    feat: add `Name` value object

---
+++ internal/domain/value_object/name.go
@@ -0,0 +1,28 @@
+package valueobject
+
+import (
+	"regexp"
+
+	"github.com/clevanilson/cs-trading-platform/pkg/errorc"
+)
+
+type Name interface {
+	Value() string
+}
+
+type name struct {
+	value string
+}
+
+func NewName(value string) (*name, error) {
+	pattern := regexp.MustCompile(`^[a-zA-Z\s]{2,255}$`)
+	isValid := pattern.MatchString(value)
+	if !isValid {
+		return nil, errorc.Newdomain("name")
+	}
+	return &name{value}, nil
+}
+
+func (n *name) Value() string {
+	return n.value
+}
---
+++ internal/domain/value_object/name_test.go
@@ -0,0 +1,35 @@
+package valueobject_test
+
+import (
+	"testing"
+
+	valueobject "github.com/clevanilson/cs-trading-platform/internal/domain/value_object"
+	"github.com/clevanilson/cs-trading-platform/pkg/assert"
+	"github.com/clevanilson/cs-trading-platform/pkg/errorc"
+)
+
+func TestName(t *testing.T) {
+	t.Run("With valid value", func(t *testing.T) {
+		assert.Each(t, []string{"Maelle", "Eren Yeager", "Noctis Lucis Caelum"}, func(value string) {
+			sut, err := valueobject.NewName(value)
+			assert.Equals(t, err, nil)
+			assert.NotEquals(t, sut, nil)
+			assert.Equals(t, sut.Value(), value)
+		})
+	})
+
+	t.Run("With invalid value", func(t *testing.T) {
+		values := []string{
+			"M@elle",
+			"",
+			"Noct1s Lucis Caelum",
+			"John_Doe",
+		}
+		assert.Each(t, values, func(value string) {
+			sut, err := valueobject.NewName(value)
+			assert.NotEquals(t, err, nil)
+			assert.Equals(t, err.Error(), errorc.Newdomain("name").Error())
+			assert.Equals(t, sut, nil)
+		})
+	})
+}
