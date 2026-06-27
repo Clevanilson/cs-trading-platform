d6c524dfa11daa979d78f9ccef2fdfd68c453378
Clevanilson <clevanilson.contato@gmail.com>
2026-06-26 21:10:15 -0300

    feat: add `HttpServer` abstraction

---
+++ pkg/server/http_server.go
@@ -0,0 +1,11 @@
+package server
+
+type HttpServer interface {
+	GET(path string, handler Handler)
+	POST(path string, handler Handler)
+	PUT(path string, handler Handler)
+	DELETE(path string, handler Handler)
+	PATCH(path string, handler Handler)
+	Start(port int) error
+	Stop() error
+}
